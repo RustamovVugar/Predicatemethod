433026dc2e60c7f9dce991700c6b21e1b9a15fa2
RustamovVugar <vugarar@code.edu.az>
2022-11-18 23:04:49 +0400

    added task1 folder

---
+++ Homeworktask1/Homeworktask1/Models/Person.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Homeworktask1.Models
+{
+    class Person
+    {
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public string Addres { get; set; }
+        public double Salary { get; set; }
+
+    }
+}
---
+++ Homeworktask1/Homeworktask1/Program.cs
@@ -0,0 +1,23 @@
+﻿using Homeworktask1.Models;
+using System;
+using System.Collections.Generic;
+
+namespace Homeworktask1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Person> people = new List<Person>();
+            people.Add(new Person { Name = "Eli", Surname = "Eliyev", Addres = "Xirdalan", Salary = 1503 });
+            people.Add(new Person { Name = "Hesen", Surname = "Nuruyev", Addres = "Sumqayit", Salary = 800 });
+            people.Add(new Person { Name = "Anar", Surname = "Ceferov", Addres = "Masazir", Salary = 2678 });
+
+            var result = people.FindAll(m => m.Salary > 1000);
+            foreach (var person in result)
+            {
+                Console.WriteLine($"{person.Name} - {person.Surname} - {person.Addres}");
+            }
+        }
+    }
+}
